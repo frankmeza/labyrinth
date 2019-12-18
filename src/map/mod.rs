912fa89e3b5559f4cb58b70992c900fb3a038226
Frank Meza <frankmezapdx@gmail.com>
2019-12-17 22:19:03 -0800

    figured out how to generate Map

--- src/map/mod.rs
+++ src/map/mod.rs
@@ -1,11 +1,33 @@
-use crate::space::{EmptySpace, ItemSpace, MinotaurSpace};
+use crate::space::{EmptySpace, ItemSpace, MinotaurSpace, Space};
 
 enum SpaceType {
-    EmptySpace,
-    ItemSpace,
-    MinotaurSpace,
+    EmptySpace(EmptySpace),
+    ItemSpace(ItemSpace),
+    MinotaurSpace(MinotaurSpace),
 }
 
 pub struct Map {
     spaces: Vec<SpaceType>,
+}
+
+fn generate_spaces() -> Vec<SpaceType> {
+    vec![
+        SpaceType::EmptySpace(EmptySpace::new("Starting Room".to_string())),
+        SpaceType::ItemSpace(ItemSpace::new("Room 1".to_string())),
+        SpaceType::EmptySpace(EmptySpace::new("Room 2".to_string())),
+        SpaceType::ItemSpace(ItemSpace::new("Room 3".to_string())),
+        SpaceType::ItemSpace(ItemSpace::new("Room 4".to_string())),
+        SpaceType::EmptySpace(EmptySpace::new("Room 5".to_string())),
+        SpaceType::ItemSpace(ItemSpace::new("Room 6".to_string())),
+        SpaceType::MinotaurSpace(MinotaurSpace::new("Final Room".to_string())),
+    ]
+}
+
+impl Map {
+    pub fn new() -> Self {
+        let spaces = generate_spaces();
+        Map {
+            spaces,
+        }
+    }
 }
