7557b268c3abe0b2ac0a5dc7094c675693078039
Frank Meza <frankmezapdx@gmail.com>
2019-12-17 18:18:25 -0800

    refactor to use new mods ascii, story

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,42 +1,10 @@
-fn print_start_message() {
-    let display = vec![
-        "*****************************************************************************",
-        "*****************************************************************************",
-        "      LOST IN A LABYRINTH...",
-        "",
-        "you find yourself inside of a huge labyrinth",
-        "the rooms are dark and damp and made of solid stone",
-        "the only way out is through...",
-        "",
-        "you are armed only with a torch, and matches to relight the torch",
-        "you must enter the labyrinth and fight the minotaur to escape",
-        "there may be items you encounter along the way that are necessary",
-        "or required to win this fight",
-        "",
-        "choose your path wisely, or you may end up lost in the labyrinth forever...",
-        "",
-        "     .   .",
-        "    /)  /)",
-        ".-.((,~,)).-,",
-        "`-,  ",
-        "   \\",
-        "   /  ,-'",
-        "  ( @   @ )",
-        "   ",
-        "     \\",
-        "     /",
-        "    -o.o-",
-        "    () ()",
-        "   ()()()()",
-        "",
-        "*****************************************************************************",
-        "*****************************************************************************",
-        "",
-    ];
+use crate::{ascii, story};
 
-    for line in &display {
-        println!("{}", line);
-    }
+fn print_start_message() {
+    println!("{}", story::star_separator());
+    println!("{}", story::lost_in_a_labyrinth());
+    println!("{}", ascii::minotaur());
+    println!("{}", story::star_separator());
 }
 
 pub fn run() {
