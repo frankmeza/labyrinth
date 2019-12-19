75f846367e182e29957e7d5bcba28eae3b165e85
Frank Meza <frankmezapdx@gmail.com>
2019-12-18 17:44:06 -0800

    add some comments for future

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,31 @@ mod space;
 mod story;
 mod utils;
 
+use game::Game;
+use map::Map;
+// use player::Player;
+
 fn main() {
     menu::display();
     let mut is_valid_choice = false;
+
+    // let player = Player::new();
+    // let game = Game::new();
+    let map = Map::new();
 
     while !is_valid_choice {
         let mut menu_choice = String::new();
         io::stdin().read_line(&mut menu_choice).unwrap().to_string();
 
         let choice: &str = menu_choice.as_str().trim();
-        utils::handle_choice(&choice, &mut is_valid_choice);
+        let will_continue = utils::handle_choice(&choice, &mut is_valid_choice);
+
+        if will_continue {
+            Game::run();
+        } else {
+            Game::quit();
+        }
+
+        // story takes over?
     }
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,20 +1,18 @@
-use crate::game;
-use std::process;
-
-pub fn handle_choice(choice: &str, is_valid_choice: &mut bool) {
+pub fn handle_choice(choice: &str, is_valid_choice: &mut bool) -> bool {
     match choice {
         "1" => {
             println!("entering game");
             *is_valid_choice = true;
-            game::run();
+            true
         }
         "2" => {
             println!("quitting game");
             *is_valid_choice = true;
-            process::exit(0);
+            false
         }
         _ => {
             println!("invalid");
+            false
         }
     }
 }
