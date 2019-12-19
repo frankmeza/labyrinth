a03804fc26c46149679ee9def0b4fb3d3224ebfb
Frank Meza <frankmezapdx@gmail.com>
2019-12-18 17:43:25 -0800

    play on, Player

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,1 +1,17 @@
-pub struct Player {}
+pub struct Player {
+    pub torch_lit: bool,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        let mut player = Player {
+            torch_lit: false,
+        };
+
+        player
+    }
+
+    pub fn get_torch_lit(&self) -> bool {
+        self.torch_lit
+    }
+}
