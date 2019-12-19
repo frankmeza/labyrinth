4740d8f7247bc29d9a25a8e9a37e263a7df50031
Frank Meza <frankmezapdx@gmail.com>
2019-12-18 17:42:32 -0800

    add new(), quit()

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,12 +1,29 @@
-use crate::{ascii, story};
+use crate::{ascii, player, story};
+use std::process;
 
-fn print_start_message() {
-    println!("{}", story::star_separator());
-    println!("{}", story::lost_in_a_labyrinth());
-    println!("{}", ascii::minotaur());
-    println!("{}", story::star_separator());
+pub struct Game {
+    player: player::Player,
 }
 
-pub fn run() {
-    print_start_message();
+impl Game {
+    pub fn new() -> Self {
+        let player = player::Player { torch_lit: true };
+
+        Game { player }
+    }
+
+    fn print_start_message() {
+        println!("{}", story::star_separator());
+        println!("{}", story::lost_in_a_labyrinth());
+        println!("{}", ascii::minotaur());
+        println!("{}", story::star_separator());
+    }
+
+    pub fn run() {
+        Game::print_start_message();
+    }
+
+    pub fn quit() {
+        process::exit(0);
+    }
 }
