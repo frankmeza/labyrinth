a4a4c80e6add1ee5fb9e1cd67d26863a0a637059
Frank Meza <frankmezapdx@gmail.com>
2019-12-23 11:22:57 -0800

    add constants

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -9,8 +9,16 @@ pub const ROOM_6: &str = "Room 6";
 pub const FINAL_ROOM: &str = "Final Room";
 
 // CHOICES
+pub const CHOICE_0: &str = "0";
 pub const CHOICE_1: &str = "1";
 pub const CHOICE_2: &str = "2";
+pub const CHOICE_3: &str = "3";
+pub const CHOICE_4: &str = "4";
+pub const CHOICE_5: &str = "5";
+
+pub const CHOICE_I: &str = "i";
+pub const CHOICE_D: &str = "d";
+pub const CHOICE_Q: &str = "q";
 
 // MENU
 pub const ENTER: &str = "entering game";
