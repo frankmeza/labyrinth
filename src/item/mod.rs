8b5edaead8831d8f09348e9c8f68e39d35589251
Frank Meza <frankmezapdx@gmail.com>
2019-12-24 17:09:37 -0800

    add all_items() -> HashMap

--- src/item/mod.rs
+++ src/item/mod.rs
@@ -1,3 +1,6 @@
+use crate::{ascii, constants};
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub struct Item {
     name: String,
@@ -6,12 +9,12 @@ pub struct Item {
 }
 
 impl Item {
-    pub fn new(
-        name: String,
-        description: String,
-        art: String,
-    ) -> Self {
-        Item { name, description, art }
+    pub fn new(name: String, description: String, art: String) -> Self {
+        Item {
+            name,
+            description,
+            art,
+        }
     }
 
     pub fn get_name(&self) -> String {
@@ -25,4 +28,56 @@ impl Item {
     pub fn get_art(&self) -> String {
         String::from(&self.art)
     }
+
+    pub fn all_items() -> HashMap<String, Item> {
+        let mut items = HashMap::new();
+
+        items.insert(
+            String::from(constants::MATCHES),
+            Item::new(
+                String::from(constants::MATCHES),
+                String::from(constants::MATCHES_DESC),
+                ascii::matches(),
+            ),
+        );
+
+        items.insert(
+            String::from(constants::ARROWS),
+            Item::new(
+                String::from(constants::ARROWS),
+                String::from(constants::ARROWS_DESC),
+                ascii::arrows(),
+            ),
+        );
+
+        items.insert(
+            String::from(constants::BOW),
+            Item::new(
+                String::from(constants::BOW),
+                String::from(constants::BOW_DESC),
+                ascii::bow(),
+            ),
+        );
+
+        items.insert(
+            String::from(constants::SHIELD),
+            Item::new(
+                String::from(constants::SHIELD),
+                String::from(constants::SHIELD_DESC),
+                ascii::shield(),
+            ),
+        );
+
+        items.insert(
+            String::from(constants::HEALTH_POTION),
+            Item::new(
+                String::from(constants::HEALTH_POTION),
+                String::from(constants::HEALTH_POTION_DESC),
+                ascii::health_potion(),
+            ),
+        );
+
+        items
+    }
+
 }
