e2c8f7475f0fbe8dd6657f1bddb9de494683c41e
Frank Meza <frankmezapdx@gmail.com>
2020-01-08 20:53:03 -0800

    use Space itself, instead of enum

--- src/map/mod.rs
+++ src/map/mod.rs
@@ -1,11 +1,11 @@
 use crate::{
     ascii, constants as c,
     player::Player,
-    space::{self, EmptySpace, ItemSpace, MinotaurSpace, Space, SpaceType},
+    space::{self, Space},
 };
 
 pub struct Map {
-    pub spaces: [SpaceType; 8],
+    pub spaces: [Space; 8],
 }
 
 impl Map {
@@ -14,27 +14,39 @@ impl Map {
         Map { spaces }
     }
 
-    pub fn get_space(&self, index: usize) -> &SpaceType {
+    pub fn get_space(&self, index: usize) -> &Space {
         &self.spaces[index]
     }
 
     pub fn get_space_by_name(&self, room_name: String) -> &Space {
         let mut iter = self.spaces.iter();
-        let found_space = &iter.find(|&st| st.get_room_name() == room_name);
+        let found_space = &iter.find(|&st| st.get_description() == room_name);
 
         // TODO handle this better
         match found_space {
-            None => self.get_space(0).get_space(),
-            Some(space_type) => space_type.get_space(),
+            None => self.get_space(0),
+            Some(space) => space,
         }
     }
 
-    pub fn enter_labyrinth(&self, player: &mut Player) {
-        let mut starting_room = self.get_space(0);
-        Map::handle_arrive_in_room(&self, &mut starting_room, player);
+    pub fn remove_items_from_space(&mut self, space: &Space) {
+         let mut iter = self.spaces.iter();
+         let found_index = iter.position(|s| &s.get_description() == &space.get_description());
+
+        match found_index {
+            None => println!("remove_items_from_space is very virus"),
+            Some(index) => {
+                self.spaces[index].items.clear();
+            }
+        }
     }
 
-    pub fn handle_arrive_in_room(&self, room: &mut SpaceType, player: &mut Player) {
+    pub fn enter_labyrinth(&self, player: &mut Player, map_ref: &Map) {
+        let starting_room = self.get_space(0);
+        Map::handle_arrive_in_room(&self, starting_room, player, map_ref);
+    }
+
+    pub fn handle_arrive_in_room(&self, room: &Space, player: &mut Player, map_ref: &Map) {
         let space = self.get_space_by_name(player.get_current_room());
 
         println!("{}", &space.get_art());
@@ -42,20 +54,20 @@ impl Map {
         println!("{}\n", ascii::lit_torch());
         println!("{}", space::get_exit_options(&space.exits));
 
-        player.set_current_room(&room.get_room_name());
-        space.do_menu(player);
+        player.set_current_room(&room.get_description());
+        Space::do_menu(player, space);
     }
 
-    fn generate_map_spaces() -> [SpaceType; 8] {
-        let space_types: [SpaceType; 8] = [
-            SpaceType::Empty(EmptySpace::new(String::from(c::STARTING_ROOM))),
-            SpaceType::Item(ItemSpace::new(String::from(c::ROOM_1))),
-            SpaceType::Empty(EmptySpace::new(String::from(c::ROOM_2))),
-            SpaceType::Item(ItemSpace::new(String::from(c::ROOM_3))),
-            SpaceType::Item(ItemSpace::new(String::from(c::ROOM_4))),
-            SpaceType::Empty(EmptySpace::new(String::from(c::ROOM_5))),
-            SpaceType::Item(ItemSpace::new(String::from(c::ROOM_6))),
-            SpaceType::Minotaur(MinotaurSpace::new(String::from(c::FINAL_ROOM))),
+    fn generate_map_spaces() -> [Space; 8] {
+        let space_types: [Space; 8] = [
+            Space::new(String::from(c::STARTING_ROOM)),
+            Space::new(String::from(c::ROOM_1)),
+            Space::new(String::from(c::ROOM_2)),
+            Space::new(String::from(c::ROOM_3)),
+            Space::new(String::from(c::ROOM_4)),
+            Space::new(String::from(c::ROOM_5)),
+            Space::new(String::from(c::ROOM_6)),
+            Space::new(String::from(c::FINAL_ROOM)),
         ];
 
         space_types
